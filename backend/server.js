b742a62e2d0ba8ab1351e87dfba82b6048514b82
Yogesh <kushawahyogesh93@gmail.com>
2024-06-17 11:35:53 +0530

    env added

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(
-  "mongodb+srv://kushawahyogesh93:Yogesh@cluster0.j9tkecq.mongodb.net/dragandrop?retryWrites=true&w=majority&appName=Cluster0");
-
+mongoose.connect(process.env.MONGO_URL);
 const itemSchema = new mongoose.Schema({
   content: String,
   position: {
